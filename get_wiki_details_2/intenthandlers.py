370911e94ce6794b365f082aed9f9bc8093bdd98
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-07-28 19:46:18 +0800

    AoB Zaira, time intent

--- get_wiki_details_2/intenthandlers.py
+++ get_wiki_details_2/intenthandlers.py
@@ -34,7 +34,19 @@ def handle_religious_practices_intent(parameters):
     pass
 
 def handle_time_intent(parameters):
-    pass
+    demonym = parameters.get("Demonym")
+    country = parameters.get("geo-country")
+    
+    demonym = demonym.lower() if demonym is not None else 'no demonym'
+    country = country.lower() if country is not None else 'no country'
+    if demonym in ("british","french") or country in ("england","france"):
+        response = "The normal working hours are 9:00am - 5:00pm. People very rarely work after office hours."
+    elif demonym in ("chinese","japanese") or country in ("china", "japan"):
+        response = "The normal working hours are 8:00am - 5:00pm. People very rarely work after office hours only on encountering high severity live incidents."
+    elif demonym == "indian" or country == "india":
+        response = "The normal working hours are 9:00am - 6:00pm. Though, people do tend to flex their working hours subject their work load and availability"
+    return utilities.generate_response_payload(response)
+
 
 def handle_weather_intent(parameters):
     pass
